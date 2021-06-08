177e3cc6242acf6548d82508a455bb6f60e65dad
kss4037 <49977437+kss4037@users.noreply.github.com>
2021-06-08 23:46:22 +0900

    Create AdminDBMgrTest class
    Create AdminDBMgrTest class

---
+++ src/data/db/AdminDBMgrTest.java
@@ -0,0 +1,40 @@
+package data.db;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AdminDBMgrTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testAdminDBMgr() {
+		assertEquals(0, 0);
+	}
+
+	@Test
+	void testGetAdminByLogin() {
+		assertEquals(0, 0);
+	}
+
+	@Test
+	void testLoginProcess() {
+		assertEquals(0, 0);
+	}
+
+}
