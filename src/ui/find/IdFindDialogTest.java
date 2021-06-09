a94945f3d86d418221663a9e27b148c536de1b05
kss4037 <49977437+kss4037@users.noreply.github.com>
2021-06-09 10:06:09 +0900

    Create IdFindDialogTest.java
    Create IdFindDialogTest.java

---
+++ src/ui/find/IdFindDialogTest.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package ui.find;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class IdFindDialogTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link ui.find.IdFindDialog#main(java.lang.String[])}.
+	 */
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ui.find.IdFindDialog#IdFindDialog()}.
+	 */
+	@Test
+	void testIdFindDialog() {
+		fail("Not yet implemented");
+	}
+
+}
