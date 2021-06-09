f0c8feee7ddf0099307bc005233c46fdcb089732
kss4037 <49977437+kss4037@users.noreply.github.com>
2021-06-09 11:03:05 +0900

    testmakeJPanel
    	/**
    	 * Purpose: set JFrame's title , icon , defaultCloseOperation
    	 * Input: makeJPanel set Jframe's title , icon as Sturbucks related. set EXIT_ON_CLOSE operation
    	 * Expected:
    	 * 		return PASS
    	 * 		Title = "스타벅스 음료 주문 프로그램"
    	 * 		Icon = "images\\logo\\로고(50x50).jpg"
    	 * 		Frame's Close operation = JFrame.EXIT_ON_CLOSE
    	 */

--- src/ui/main/MainFrameTest.java
+++ src/ui/main/MainFrameTest.java
@@ -2,8 +2,11 @@ package ui.main;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.Toolkit;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import org.junit.jupiter.api.AfterEach;
@@ -17,10 +20,6 @@ class MainFrameTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		testMainFrame = new MainFrame();
-		Method method = testMainFrame.getClass().getDeclaredMethod("setJframe");
-		method.setAccessible(true);
-		method.invoke(testMainFrame);
-		System.out.print(testMainFrame.getTitle());
 
 	}
 
@@ -28,12 +27,23 @@ class MainFrameTest {
 	void tearDown() throws Exception {
 		testMainFrame = null;
 	}
-
+	/**
+	 * Purpose: set JFrame's title , icon , defaultCloseOperation
+	 * Input: makeJPanel set Jframe's title , icon as Sturbucks related. set EXIT_ON_CLOSE operation
+	 * Expected:
+	 * 		return PASS
+	 * 		Title = "스타벅스 음료 주문 프로그램"
+	 * 		Icon = "images\\logo\\로고(50x50).jpg"
+	 * 		Frame's Close operation = JFrame.EXIT_ON_CLOSE
+	 */
 	@Test
-	void testMainFrame() {
-	}
-	
-	@Test
-	void testmakeJPanel() {
+	void testmakeJPanel() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Method method = testMainFrame.getClass().getDeclaredMethod("setJframe");
+		method.setAccessible(true);
+		method.invoke(testMainFrame);
+		
+		assertEquals("스타벅스 음료 주문 프로그램", testMainFrame.getTitle());
+		assertEquals(Toolkit.getDefaultToolkit().getImage("images\\logo\\로고(50x50).jpg"), testMainFrame.getIconImage());
+		assertEquals(testMainFrame.getDefaultCloseOperation(),JFrame.EXIT_ON_CLOSE);
 	}
 }
